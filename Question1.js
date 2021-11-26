74f9cbd7ca2413adfbc4ee079bff4636b55d7f01
Abhishekchandra958 <90219230+Abhishekchandra958@users.noreply.github.com>
2021-11-26 12:19:10 +0530

    Add files via upload

---
+++ Question1.js
@@ -0,0 +1,19 @@
+var Person1 ={
+    FirstName : "Abhishek",
+    LastName : "Chandra",
+    OfficeAddress : "Great Noida, Delhi NCR",
+  }
+  var Person2 ={
+    FirstName : "Avinash",
+    LastName : "Chandra",
+    OfficeAddress : "Muzaffarpur, Bihar",
+  }
+  var Person3 ={
+    FirstName : "Aditya",
+    LastName : "Chandra",
+    OfficeAddress : "Kota, Rajashthan",
+  }
+  
+  console.log("Name of First Person : "+Person1.FirstName+" "+Person1.LastName+"     First Person's Address : "+Person1.OfficeAddress);
+  console.log("Name of Second Person : "+Person2.FirstName+" "+Person2.LastName+"    Second Person's Address : "+Person2.OfficeAddress);
+  console.log("Name of Third Person : "+Person3.FirstName+" "+Person3.LastName+"     Third Person's Address : "+Person3.OfficeAddress);
